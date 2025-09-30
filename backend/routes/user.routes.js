b332f15aa5f2015e45a4e28eb915d054ce6a27ee
AlgoAnurag <139620674+AlgoAnurag@users.noreply.github.com>
2025-09-30 14:26:08 +0530

    final project commit

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { editProfile, getCurrentUser, getOtherUsers } from "../controllers/user.controllers.js"
+import { editProfile, getCurrentUser, getOtherUsers, search } from "../controllers/user.controllers.js"
 import isAuth from "../middlewares/isAuth.js"
 import { upload } from "../middlewares/multer.js"
 
@@ -9,6 +9,6 @@ const userRouter = express.Router()
 userRouter.get("/current",isAuth,getCurrentUser)
 userRouter.put("/profile",isAuth,upload.single("image"),editProfile)
 userRouter.get("/others",isAuth,getOtherUsers)
-
+userRouter.get("/search",isAuth, search) 
 
 export default userRouter
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
@@ -7,20 +7,49 @@ import Profile from './pages/Profile'
 import Home from './pages/Home'
 import getCurrentUser from './customHooks/getCurrentUser'
 import getOtherUsers from './customHooks/getOtherusers'
+import { io } from "socket.io-client"
+import { serverUrl } from './main'
+import { setOnlineUsers, setSocket } from './redux/userSlice'
 
 
 
 function App() {
   getCurrentUser()
   getOtherUsers()
-  let {userData}=useSelector(state=>state.user) 
+  let { userData, socket, onlineUsers } = useSelector(state => state.user)
   let dispatch = useDispatch()
+
+  useEffect(() => {
+    if (userData) {
+      const socketio = io(`${serverUrl}`, {
+        query: {
+          userId: userData?._id
+        }
+      })
+      dispatch(setSocket(socketio))
+
+      socketio.on("getOnlineUsers", (users) => {
+        dispatch(setOnlineUsers(users))
+      })
+
+      return () => socketio.close()
+
+    } else {
+      if (socket) {
+        socket.close()
+        dispatch(setSocket(null))
+      }
+    }
+
+
+  }, [userData])
+
   return (
     <Routes>
-      <Route path='/login' element={!userData?<Login/>:<Navigate to="/"/>}/>
-      <Route path='/signup' element={!userData?<Signup/>:<Navigate to="/profile"/>}/>
-      <Route path='/' element={userData?<Home/>:<Navigate to="/login"/>}/>
-      <Route path='/profile' element={userData?<Profile/>:<Navigate to="/signup"/>}/>
+      <Route path='/login' element={!userData ? <Login /> : <Navigate to="/" />} />
+      <Route path='/signup' element={!userData ? <Signup /> : <Navigate to="/profile" />} />
+      <Route path='/' element={userData ? <Home /> : <Navigate to="/login" />} />
+      <Route path='/profile' element={userData ? <Profile /> : <Navigate to="/signup" />} />
     </Routes>
   )
 }
